ed80bbf408ba94cfb8fc1d77176398001a165924
Joe Zuntz <joezuntz@googlemail.com>
2018-02-06 07:19:02 -0800

    Allow passing pre-constructed ini and pipeline to run_cosmosis

--- cosmosis/main.py
+++ cosmosis/main.py
@@ -87,46 +87,27 @@ def demo_10_special (args):
         os.environ ["HALOFIT"] = "halofit"
 
 
-def demo_20a_special (args):
-    if  "demo20a.ini" in args.inifile:
-        print ()
-        print ("You have completed demo20a, now run demo20b and compare")
-        print ("results with demo5!")
-
-
-def demo_20b_special (args):
-    if   "demo20b.ini" in args.inifile   and   not os.path.isfile ("./demo20a.txt"):
-        print ()
-        print ("********************************************************")
-        print ("*** YOU MUST RUN demo20a BEFORE YOU CAN RUN demo20b. ***")
-        print ("********************************************************")
-        
-
-def run_cosmosis(args, pool=None):
+def run_cosmosis(args, pool=None, ini=None, pipeline=None):
 
     # In case we need to hand-hold a naive demo-10 user.
     demo_10_special (args)
     demo_20b_special (args)
 
     # Load configuration.
-    ini = Inifile(args.inifile, override=args.params)
+    if ini is None:
+        ini = Inifile(args.inifile, override=args.params)
 
 
 
     # Create pipeline.
-    pool_stdout = ini.getboolean(RUNTIME_INI_SECTION, "pool_stdout", fallback=False)
-    if (pool is None) or pool.is_master() or pool_stdout:
-        pipeline = LikelihoodPipeline(ini, override=args.variables)
-        if pipeline.do_fast_slow:
-            pipeline.setup_fast_subspaces()
-
-    else:
-        # Suppress output on everything except the master process
-        with stdout_redirected():
+    if pipeline is None:
+        pool_stdout = ini.getboolean(RUNTIME_INI_SECTION, "pool_stdout", fallback=False)
+        if (pool is None) or pool.is_master() or pool_stdout:
             pipeline = LikelihoodPipeline(ini, override=args.variables) 
-            if pipeline.do_fast_slow:
-                pipeline.setup_fast_subspaces()
-
+        else:
+            # Suppress output on everything except the master process
+            with stdout_redirected():
+                pipeline = LikelihoodPipeline(ini, override=args.variables) 
 
     # determine the type(s) of sampling we want.
     sample_methods = ini.get(RUNTIME_INI_SECTION, "sampler", fallback="test").split()
