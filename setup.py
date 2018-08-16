b60d50fdf788f1354deb89d026b989e1ffcd3898
Tilman Troester <tilman.troester@gmail.com>
2018-08-16 13:51:46 +0100

    Cleaned up setup.py and datablock Makefile

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
-#from numpy.distutils.core import Extension
-#from distutils.core import Extension
 from setuptools import setup, find_packages, Extension
 from distutils.command.install import install
 from distutils.command.build import build
 import os
 
-version = '0.0.7'
+version = '0.0.8'
 
-cc_files = [
-    "cosmosis/datablock/c_datablock.cc",
-    "cosmosis/datablock/datablock_logging.cc",
-    "cosmosis/datablock/entry.cc",
-    "cosmosis/datablock/section.cc",
-    "cosmosis/datablock/datablock.cc",
-]
-
-
-f90_files = [
-    "cosmosis_section_names.F90",
-    "cosmosis_types.F90",
-    "cosmosis_wrappers.F90",
-    "cosmosis_modules.F90",
+f90_mods = [
+    "cosmosis/datablock/cosmosis_section_names.mod",
+    "cosmosis/datablock/cosmosis_types.mod",
+    "cosmosis/datablock/cosmosis_wrappers.mod",
+    "cosmosis/datablock/cosmosis_modules.mod",
 ]
 
 scripts = [
@@ -54,9 +43,10 @@ cc_headers = [
     "cosmosis/datablock/section.h"
 ]
 
-
-f90_objects = [f[:-4]+".o" for f in f90_files]
-f90_headers = [f[:-4]+".mod" for f in f90_files]
+if sys.platform == 'darwin':
+    from distutils import sysconfig
+    vars = sysconfig.get_config_vars()
+    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
 def compile_library():
     cosmosis_src_dir = os.getcwd()
@@ -124,7 +114,7 @@ if __name__ == "__main__":
           author_email      = "joezuntz@googlemail.com",
           ext_modules = [ext1],
           packages = find_packages(),
-          include_package_data = True,
+          package_data = {"" : ["datablock/libcosmosis.so",] + c_headers + cc_headers + f90_mods},
           scripts = scripts,
           install_requires = ['pyyaml', 'future', 'configparser', 'emcee', 'numpy', 'scipy'],
           cmdclass={"install":my_install, "build":my_build},
